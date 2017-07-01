b3b747e5908447a1e3678e3db83f7cca14942580
Allen Qiu <aqiu384@gmail.com>
2017-07-01 10:57:42 -0700

    persistant dlc list, hidden tabs removed

--- src/smt4a-tool/P5Tool.js
+++ src/smt4a-tool/P5Tool.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
 import SimpleTabs, { SimpleTabNavbar } from './common/SimpleTabs'
-import DemonEntry from './demon-entry/DemonEntry'
-import DemonTable from './DemonTable'
+import PersonaTable from './PersonaTable'
 import SkillTable from './SkillTable'
+import PersonaEntry from './persona-entry/PersonaEntry'
 import DlcTable from './DlcTable'
 import Footer from './Footer'
 
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import DlcDemons from './data/DlcDemons'
-import './Smt4ATool.css'
+import DlcPersonas from './data/DlcPersonas'
+import './P5Tool.css'
 
-const HAS_DLC_KEY = 'smt4a-tool/hasDlc'
+const HAS_DLC_KEY = 'p5-tool/hasDlc'
 
-class Smt4ATool extends React.PureComponent {
+class P5Tool extends React.PureComponent {
   constructor(props) {
     super(props)
 
     let hasDlc = JSON.parse(localStorage.getItem(HAS_DLC_KEY))
     if (!hasDlc) {
-      hasDlc = DlcDemons.reduce( (acc, dlc) => { acc[dlc] = false; return acc }, {} )
+      hasDlc = DlcPersonas.reduce( (acc, dlc) => { acc[dlc] = false; return acc }, {} )
       localStorage.clear()
       localStorage.setItem(HAS_DLC_KEY, JSON.stringify(hasDlc))
     }
@@ -51,26 +51,26 @@ class Smt4ATool extends React.PureComponent {
   }
 
   render() {
-    const { match } = this.props
+    const { match, location } = this.props
     const { hasDlc } = this.state
 
     const baseUrl = match.url === '/' ? '' : match.url
-    const demonsUrl = baseUrl + '/demons'
+    const personasUrl = baseUrl + '/personas'
 
     const tabs = [
       {
-        name: 'demons',
-        label: 'List of Demons',
-        content: <DemonTable {...{ demonsUrl, match, hasDlc }}/>
+        name: 'personas',
+        label: 'List of Personas',
+        content: <PersonaTable {...{ personasUrl, match, hasDlc }}/>
       },
       {
         name: 'skills',
         label: 'List of Skills',
-        content: <SkillTable {...{ demonsUrl: demonsUrl, match, hasDlc }}/>
+        content: <SkillTable {...{ personasUrl, match }}/>
       },
       {
         name: 'dlc',
-        label: 'Add DLC Demons',
+        label: 'Enter Purchased DLC',
         content: <DlcTable {...{ initHasDlc: hasDlc, onSubmit: this.onDlcSubmit }}/>
       }
     ]
@@ -79,21 +79,31 @@ class Smt4ATool extends React.PureComponent {
 
     return (
       <Switch>
-        <Route exact path={match.url} render={ () => <Redirect to={demonsUrl}/> }/>
-        <Route path="/index.html" render={ () => <Redirect to={`${baseUrl}/demons`}/> }/> 
+        <Route exact path={match.url} render={ () => <Redirect to={personasUrl}/> }/>
+        <Route path="/index.html" render={ () => <Redirect to={`${baseUrl}/personas`}/> }/> 
         <Route path="/skills.html" render={ () => <Redirect to={`${baseUrl}/skills`}/> }/> 
-        <Route path="/demons.html" render={ () => <Redirect to={`${baseUrl}/demons`}/> }/> 
+        <Route path="/personas.html" render={ () => {
+          const nameParam = 'persona'
+          const nameBegin = location.search.indexOf(nameParam + '=')
+
+          let name = ''
+          if (nameBegin !== -1) {
+            const nameSuffix = location.search.substring(nameBegin + nameParam.length) + '&'
+            console.log(nameSuffix)
+            name = '/' + nameSuffix.substring(1, nameSuffix.indexOf('&'))
+          }
+
+          return <Redirect to={`${baseUrl}/personas${name}`}/>
+        } }/> 
         <Route path={`${baseUrl}/:tab`} render={ ({ match }) => (
           <div className="compendium">
             <div className={match.isExact ? 'show' : 'hide'}>
               <SimpleTabs currTab={match.params.tab} tabs={tabs} baseUrl={baseUrl}/>
             </div>
-            { match.params.tab === 'demons' &&
-              <Route path={`${match.url}/:name`} render={ ({ match }) => 
-                <DemonEntry {...{
-                  tabLinks, match, hasDlc,
-                  demonsUrl: demonsUrl
-                }}/> 
+            { match.params.tab === 'personas' &&
+              <Route path={`${match.url}/:name`} render={ ({ match, location }) => 
+                <PersonaEntry tabLinks={tabLinks} location={location} 
+                personasUrl={personasUrl} match={match} hasDlc={hasDlc}/> 
               }/>
             }
             <Footer/>
@@ -107,7 +117,7 @@ class Smt4ATool extends React.PureComponent {
 function App() {
   return (
     <Router basename={process.env.REACT_APP_BASE_PATHNAME}>
-      <Route path="/" component={Smt4ATool}/>
+      <Route path="/" component={P5Tool}/>
     </Router>
   )
 }
